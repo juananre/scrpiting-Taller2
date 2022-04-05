5d7f0493c05aad2a11101aa72982a74ff3758048
juananre <78058130+juananre@users.noreply.github.com>
2022-04-04 20:52:32 -0500

    hola

---
+++ proyect/unita.cs
@@ -0,0 +1,58 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+
+namespace proyect
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void crear_carta()
+        {
+            //vectores 
+            string name= Aut
+            
+
+            //
+            
+        }
+
+        [TestMethod]
+        public void crear_jugador()
+        {
+            
+        }
+
+        [TestMethod]
+        public void cear_barajaDECK()
+        {
+            
+        }
+
+        [TestMethod]
+        public void prueva_Character()
+        {
+            
+            
+        }
+
+        [TestMethod]
+        public void prueva_Equip()
+        {
+            
+        }
+
+        [TestMethod]
+        public void prueva_SupportSkill()
+        {
+           
+        }
+
+        [TestMethod]
+        public void cartas_destruidas()
+        {
+           
+        }
+    }
+}
